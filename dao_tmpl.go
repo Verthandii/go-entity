25708a1492938f8f4abe41338379a1acbf607a6c
王真 <1046060018@qq.com>
2022-01-06 16:08:05 +0800

    feat: dao 命名不再添加 Dao

--- dao_tmpl.go
+++ dao_tmpl.go
@@ -8,37 +8,37 @@ import (
 	"gorm.io/gorm"
 )
 
-type {{ $TableName }}Dao struct{}
+type {{ $TableName }} struct{}
 
-func New{{ $TableName }}Dao() *{{ $TableName }}Dao {
-	return &{{ $TableName }}Dao{}
+func New{{ $TableName }}() *{{ $TableName }} {
+	return &{{ $TableName }}{}
 }
 
-func (*{{ $TableName }}Dao) Transaction(db *gorm.DB, fc func(tx *gorm.DB) error) error {
+func (*{{ $TableName }}) Transaction(db *gorm.DB, fc func(tx *gorm.DB) error) error {
 	return db.Transaction(fc)
 }
 
 // 增
 
-func (*{{ $TableName }}Dao) Create(tx *gorm.DB, v interface{}) error {
+func (*{{ $TableName }}) Create(tx *gorm.DB, v interface{}) error {
 	return querypath.New{{ $TableName }}(tx).Create(v)
 }
 
 // 删
 
-func (*{{ $TableName }}Dao) DeleteById(tx *gorm.DB, id int) error {
+func (*{{ $TableName }}) DeleteById(tx *gorm.DB, id int) error {
 	return querypath.New{{ $TableName }}(tx).WhIdEq(id).Delete(&model.{{ $TableName }}{})
 }
 
 // 改
 
-func (*{{ $TableName }}Dao) UpdateById(tx *gorm.DB, id int, v interface{}) error {
+func (*{{ $TableName }}) UpdateById(tx *gorm.DB, id int, v interface{}) error {
 	return querypath.New{{ $TableName }}(tx).WhIdEq(id).Update(v)
 }
 
 // 查
 
-func (*{{ $TableName }}Dao) List(q *querypath.{{ $TableName }}) (model.{{ $TableName }}Slice, error) {
+func (*{{ $TableName }}) List(q *querypath.{{ $TableName }}) (model.{{ $TableName }}Slice, error) {
 	vs := make(model.{{ $TableName }}Slice, 0)
 	if err := q.Find(&vs); err != nil {
 		return nil, err
@@ -47,7 +47,7 @@ func (*{{ $TableName }}Dao) List(q *querypath.{{ $TableName }}) (model.{{ $Table
 	return vs, nil
 }
 
-func (*{{ $TableName }}Dao) ListPagination(q *querypath.{{ $TableName }}, page, size int) (model.{{ $TableName }}Slice, *querypath.Paginate, error) {
+func (*{{ $TableName }}) ListPagination(q *querypath.{{ $TableName }}, page, size int) (model.{{ $TableName }}Slice, *querypath.Paginate, error) {
 	vs := make(model.{{ $TableName }}Slice, 0)
 	p, err := q.SetPaginate(&querypath.Paginate{
 		Page: page,
@@ -60,7 +60,7 @@ func (*{{ $TableName }}Dao) ListPagination(q *querypath.{{ $TableName }}, page, 
 	return vs, p, nil
 }
 
-func (*{{ $TableName }}Dao) First(q *querypath.{{ $TableName }}) (*model.{{ $TableName }}, error) {
+func (*{{ $TableName }}) First(q *querypath.{{ $TableName }}) (*model.{{ $TableName }}, error) {
 	v := &model.{{ $TableName }}{}
 	if err := q.First(v); err != nil {
 		return nil, err
@@ -69,7 +69,7 @@ func (*{{ $TableName }}Dao) First(q *querypath.{{ $TableName }}) (*model.{{ $Tab
 	return v, nil
 }
 
-func (*{{ $TableName }}Dao) FirstById(tx *gorm.DB, id int) (*model.{{ $TableName }}, error) {
+func (*{{ $TableName }}) FirstById(tx *gorm.DB, id int) (*model.{{ $TableName }}, error) {
 	v := &model.{{ $TableName }}{}
 	if err := querypath.New{{ $TableName }}(tx).WhIdEq(id).First(v); err != nil {
 		return nil, err
